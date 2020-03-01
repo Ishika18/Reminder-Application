7c7af269a4a8086457f0744e3fe23d05c32406fe
ronliang6 <ronaldliang889@gmail.com>
2020-02-29 22:42:55 -0800

    changed darkSky.js to work with axios instead of node fetch. Async/await is simpler than node fetch, but all functions that handle darkSky API must be async, which is relatively simple to implement

--- darkSky.js
+++ darkSky.js
@@ -1,41 +1,31 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 let darksky = 'https://api.darksky.net/forecast/';
 let key = '55a1b0aafc740393ca2506b2ab5311dc';
-let lat = 49.2298125; // van
-let long = -123.17074910000001; // van
+let lat = 49.3; // van
+let long = -123.1; // van
 //let time = 1582992000 // feb 29 2020 4pm
 let uri = darksky + key + '/' + lat +','+ long; // + "," + time;
 uri = uri.concat('?units=ca&exclude=minutely,hourly,alerts,flags,currently');
     
-let options = {
-    method: 'GET',
-    mode: 'cors'
-}
-let req = new fetch.Request(uri, options);
+var rainChance = []
 
-function getRainProbabilities(){
-    let rainChance = []
-    fetch(req)
-        .then((response)=>{
-            if(response.ok){
-                return response.json();
-            }else{
-                throw new Error('Bad HTTP!')
-            }
-        })
-        .then((j) =>{
-            for (let i = 0; i < 8; i++){
-                rainChance[i] =j.daily.data[i].precipProbability
-            }
-            console.log(j.daily.data)//.precipProbability);
-            console.log(rainChance)
-            return rainChance
-            //console.log('JSON data provided');
-        })
-        .catch( (err) =>{
-            console.log('ERROR:', err.message);
-        });
-    
+async function getRainProbability() {
+    rainChance = [];
+    try {
+        const darkData = await axios(uri);
+        for(let i = 0; i < 8; i++){
+            rainChance[i] = darkData.data.daily.data[i].precipProbability;
+        }
+        return rainChance;     
+    } catch (error) {
+      console.error(error);
+    }
+  } 
+
+
+async function myFunc() {
+    let x = await getRainProbability()
+    console.log(x)
 }
-console.log("hello")
-console.log(getRainProbabilities())
+
+myFunc()
