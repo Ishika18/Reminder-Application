212f3df818646cf163a71bf0695f90e5c1260142
Ishika18 <shagunphw@gmail.com>
2020-03-19 23:15:29 -0700

    added a sign up page, user sign up and sign in using firabase backend(still buggy)

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-function mainPage(){
+let userId = document.getElementById("username");
+let password = document.getElementById("password");
+
+function mainPage() {
     // logic for user login
+    const id = userId.value;
+    const pass = password.value;
+    const auth = firebase.auth();
+    // Sign in
+    const promise = auth.signInWithEmailAndPassword(id, pass);
+    // if their is a user log them in if not catch the error
+    promise.catch(e => console.log(e.message));
+
     location.href = "index.html";
+    realtimeListener();
+}
+
+function realtimeListener() {
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if(firebaseUser) {
+            console.log(firebaseUser);
+        } else {
+            console.log('not logged in');
+        }
+    })
 }
