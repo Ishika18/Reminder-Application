772bbdc84e97ec295b47ce080c711780d4b78d92
Ishika18 <shagunphw@gmail.com>
2020-03-24 10:41:13 -0700

    user login functionality

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
-let userId = document.getElementById("username");
-let password = document.getElementById("password");
+const auth = firebase.auth();
 
-function mainPage() {
-    // logic for user login
+function login() {
+    let userId = document.getElementById("emailId");
+    let password = document.getElementById("password");
     const id = userId.value;
     const pass = password.value;
-    const auth = firebase.auth();
-    // Sign in
-    const promise = auth.signInWithEmailAndPassword(id, pass);
+    // logic for user login
+    const promise = auth.signInWithEmailAndPassword(id.trim(), pass);
     // if their is a user log them in if not catch the error
     promise.catch(e => console.log(e.message));
 
-    location.href = "index.html";
+    //location.href = "index.html";
     realtimeListener();
+}
+
+function signUp() {
+    let userId = document.getElementById("emailId");
+    let password = document.getElementById("password");
+    const id = userId.value;
+    const pass = password.value;
+    // Creates the user and sign in
+    const promise = auth.createUserWithEmailAndPassword(id.trim(), pass);
+    // if their is a user log them in if not catch the error
+    promise.catch(e => console.log(e.message));
+
+    // login the user after creating the id.
 }
 
 function realtimeListener() {
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if(firebaseUser) {
+            // if the user is logged in.
             console.log(firebaseUser);
         } else {
             console.log('not logged in');
