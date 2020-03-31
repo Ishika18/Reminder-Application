80ebd2fb16629a7280ff10cf507993e522ff0a65
Blake Michalzik <bmichalzik@gmail.com>
2020-03-31 13:58:18 -0700

    Blake - Completed app navbar.

--- public/js/landing_ejs.js
+++ public/js/landing_ejs.js
@@ -1,1 +1,16 @@
 // bm - js for landing page
+
+// reminder application login, for now just forwards to app page
+function reminderLogin() {
+    document.location = "/reminder/single"
+}
+
+// reminder application login via google
+function googleLogin() {
+
+}
+
+// reminder application login via facebook
+function facebookLogin() {
+
+}
