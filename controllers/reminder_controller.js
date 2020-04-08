8f076de8e67d94e77074ff65107fdb43eb8e4092
Ishika18 <shagunphw@gmail.com>
2020-04-07 21:44:10 -0700

    added cdn links in layout.ejs

--- controllers/reminder_controller.js
+++ controllers/reminder_controller.js
@@ -1,5 +1,8 @@
 let Database = require("../database");
 const fetch = require('node-fetch');
+const auth = firebase.auth();
+const database = firebase.firestore();
+
 
 let remindersController = {
 
